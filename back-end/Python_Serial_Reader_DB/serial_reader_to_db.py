95e4952ee33809ad6af5ae1826f5dc63439e93c1
Shazlow <boss2457bc@gmail.com>
2025-11-06 20:12:32 +0700

    upload gateway serial reader to database

---
+++ back-end/Python_Serial_Reader_DB/serial_reader_to_db.py
@@ -0,0 +1,54 @@
+import serial.tools.list_ports
+import serial
+import json
+import psycopg2
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+ports = serial.tools.list_ports.comports()
+port = None
+for p in ports:
+    port = p.device
+    print(p.device)
+
+ser = serial.Serial(port, 115200, timeout=1)
+
+conn = psycopg2.connect(
+    host=os.getenv("POSTGRES_HOST"),
+    dbname=os.getenv("POSTGRES_DB"),
+    user=os.getenv("POSTGRES_USER"),
+    password=os.getenv("POSTGRES_PASSWORD"),
+    port=os.getenv("POSTGRES_PORT")
+)
+
+cur = conn.cursor()
+
+counter = 0
+execute_interval = 1
+while True:
+    line = ser.readline().decode().strip()
+    if not line:
+        continue
+    start = line.find("{")
+    end = line.rfind("}")
+
+    if start == -1 or end == -1:
+        continue
+    json_str = line[start:end+1]
+    try:
+        data = json.loads(json_str)
+        print(data)
+        #print(f"HelmetID:{type(data['HelmetID'])}, AvgBPM:{type(data['AvgBPM'])}, TemperatureC:{type(data['TemperatureC'])}")
+        sql = "INSERT INTO sensor_data (heart_rate, body_temp, helmet_id) VALUES (%s, %s, %s)"
+        cur.execute(sql, (data["AvgBPM"], data["TemperatureC"], data["HelmetID"]))
+        counter += 1
+        if counter % execute_interval == 0:
+            conn.commit()
+    except json.JSONDecodeError:
+        print("bad json:", line)
+    except Exception as e:
+        print("Error:", e)
+
+    
