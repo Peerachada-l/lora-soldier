ede113d36bb3bf93b97c4bbdcbfcb90bcfdded09
Shazlow <boss2457bc@gmail.com>
2025-11-07 17:13:19 +0700

    change psycopg2 to requests to trigger websocket through api

--- back-end/Python_Serial_Reader_DB/serial_reader_to_db.py
+++ back-end/Python_Serial_Reader_DB/serial_reader_to_db.py
@@ -3,9 +3,12 @@ import serial
 import json
 import psycopg2
 from dotenv import load_dotenv
+import requests
 import os
 
 load_dotenv()
+
+url = "http://localhost:8000/sensors/"
 
 ports = serial.tools.list_ports.comports()
 port = None
@@ -15,18 +18,18 @@ for p in ports:
 
 ser = serial.Serial(port, 115200, timeout=1)
 
-conn = psycopg2.connect(
-    host=os.getenv("POSTGRES_HOST"),
-    dbname=os.getenv("POSTGRES_DB"),
-    user=os.getenv("POSTGRES_USER"),
-    password=os.getenv("POSTGRES_PASSWORD"),
-    port=os.getenv("POSTGRES_PORT")
-)
+# conn = psycopg2.connect(
+#     host=os.getenv("POSTGRES_HOST"),
+#     dbname=os.getenv("POSTGRES_DB"),
+#     user=os.getenv("POSTGRES_USER"),
+#     password=os.getenv("POSTGRES_PASSWORD"),
+#     port=os.getenv("POSTGRES_PORT")
+# )
 
-cur = conn.cursor()
+# cur = conn.cursor()
 
-counter = 0
-execute_interval = 1
+# counter = 0
+# execute_interval = 1
 while True:
     line = ser.readline().decode().strip()
     if not line:
@@ -39,13 +42,19 @@ while True:
     json_str = line[start:end+1]
     try:
         data = json.loads(json_str)
+        payload = {
+            "heart_rate": data["AvgBPM"],
+            "body_temp": data["TemperatureC"],
+            "fall_detected": False
+        }
         print(data)
+        requests.post(url+str(data['HelmetID']), json=payload)
         #print(f"HelmetID:{type(data['HelmetID'])}, AvgBPM:{type(data['AvgBPM'])}, TemperatureC:{type(data['TemperatureC'])}")
-        sql = "INSERT INTO sensor_data (heart_rate, body_temp, helmet_id) VALUES (%s, %s, %s)"
-        cur.execute(sql, (data["AvgBPM"], data["TemperatureC"], data["HelmetID"]))
-        counter += 1
-        if counter % execute_interval == 0:
-            conn.commit()
+        #sql = "INSERT INTO sensor_data (heart_rate, body_temp, helmet_id) VALUES (%s, %s, %s)"
+        #cur.execute(sql, (data["AvgBPM"], data["TemperatureC"], data["HelmetID"]))
+        #counter += 1
+        #if counter % execute_interval == 0:
+        #    conn.commit()
     except json.JSONDecodeError:
         print("bad json:", line)
     except Exception as e:
